ac2a0c2cf1bb13ab3e79e8d204605721f5711272
Jonwil <wilsonchay201830530@cunoc.edu.gt>
2023-12-18 22:42:28 -0600

    Clase que genera el archivo.dot

---
+++ Reproductor/generar_reporte.py
@@ -0,0 +1,20 @@
+
+from graphviz import Digraph    
+    
+def generar_grafo(canciones, nombre_archivo_dot):
+    dot = Digraph(comment='Grafo de Cancionsssss')
+
+    actual = canciones.head
+    while actual:
+        canciones = actual.canciones
+        dot.node(canciones.nombre, f"{canciones.nombre}")
+        actual = actual.tail
+
+    # Conectar nodos en función de la lista enlazada
+    actual = canciones.head
+    while actual and actual.tail:
+        dot.edge(actual.canciones.nombre, actual.tail.canciones.nombre)
+        actual = actual.tail
+
+    # Guardar el archivo DOT
+    dot.save(nombre_archivo_dot)
